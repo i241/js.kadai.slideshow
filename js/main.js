678f57b7e1c2687ad8a0266a8b36026bdb3a4ac3
EC2 Default User <ec2-user@ip-172-31-74-148.ec2.internal>
2021-05-09 12:13:27 +0000

    first

---
+++ js/main.js
@@ -0,0 +1,7 @@
+$('.slick').slick({
+  autoplay:true,
+  autoplaySpeed:1000,
+  arrows: true,
+  prevArrow: '<button class="slide-arrow prev-arrow"></button>',
+  nextArrow: '<button class="slide-arrow next-arrow"></button>',
+});
